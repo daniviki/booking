e483b6ad78fc33c9a3dddffb89ff59c4f8e3191a
caklipakli <caklipakli@gmail.com>
2019-05-23 11:36:01 +0200

    JWT authentication filter added

--- src/main/java/com/chrysoprase/booking/MainController.java
+++ src/main/java/com/chrysoprase/booking/MainController.java
@@ -1,11 +1,17 @@
 package com.chrysoprase.booking;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.xml.bind.DatatypeConverterImpl;
+
 @Controller
 public class MainController {
+
+  private BCryptPasswordEncoder bCryptPasswordEncoder;
+
 
   @PostMapping("/sign-up")
   public void signUp(@RequestBody ApplicationUser user) {
