2695878a0e4f9b7c2127ee51a61f7d57efc0c018
caklipakli <caklipakli@gmail.com>
2019-05-23 11:43:00 +0200

    JWT authorization filter added

--- src/main/java/com/chrysoprase/booking/security/JwtAuthenticationFilter.java
+++ src/main/java/com/chrysoprase/booking/security/JwtAuthenticationFilter.java
@@ -1,10 +1,12 @@
 package com.chrysoprase.booking.security;
 
+import com.auth0.jwt.JWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -13,6 +15,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
+
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+import static com.chrysoprase.booking.security.SecurityConstants.EXPIRATION_TIME;
+import static com.chrysoprase.booking.security.SecurityConstants.HEADER_STRING;
+import static com.chrysoprase.booking.security.SecurityConstants.SECRET;
+import static com.chrysoprase.booking.security.SecurityConstants.TOKEN_PREFIX;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
   private AuthenticationManager authenticationManager;
