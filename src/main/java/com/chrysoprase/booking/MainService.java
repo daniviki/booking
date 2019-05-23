3ca2c6ade83a685e4942343bba811d8f083fd221
caklipakli <caklipakli@gmail.com>
2019-05-23 20:59:10 +0200

    Minor changes

--- src/main/java/com/chrysoprase/booking/MainService.java
+++ src/main/java/com/chrysoprase/booking/MainService.java
@@ -6,6 +6,7 @@ import com.chrysoprase.booking.appuser.AppUserService;
 import com.chrysoprase.booking.exception.WrongPasswordException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
