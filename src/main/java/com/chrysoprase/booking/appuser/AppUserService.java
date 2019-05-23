95408831e1d246ab9c199186a6422805cfa00a20
Viki <daniviki3@gmail.com>
2019-05-23 15:34:54 +0200

    update appUserService: add methods for login

--- src/main/java/com/chrysoprase/booking/appuser/AppUserService.java
+++ src/main/java/com/chrysoprase/booking/appuser/AppUserService.java
@@ -1,6 +1,9 @@
 package com.chrysoprase.booking.appuser;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,5 +26,23 @@ public class AppUserService {
 
   public void saveUser(AppUser user) {
     appUserRepository.save(user);
+  }
+
+  public boolean isDatabaseContainsUsername(String username) {
+    return appUserRepository.existsAppUserByUsername(username);
+  }
+
+  public String missingParameter(AppUser user) {
+    String parameters = "";
+    if (user.getUsername() == null && user.getPassword() == null) {
+      return parameters = "username, password";
+    }
+    if (user.getUsername() == null) {
+      return parameters = "username";
+    }
+    if (user.getPassword() == null) {
+      return parameters = "password";
+    }
+    return parameters;
   }
 }
