d4a508a36a3dcd152cf7f641be7ad772687ec6b1
Karoly Valentin <valentin.karoly@gmail.com>
2019-05-23 12:46:27 +0200

    employee service with basic function

---
+++ src/main/java/com/chrysoprase/booking/employee/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.chrysoprase.booking.employee;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+
+  private EmployeeRepository employeeRepository;
+
+  @Autowired
+  public EmployeeService(EmployeeRepository employeeRepository) {
+    this.employeeRepository = employeeRepository;
+  }
+
+  private Employee findEmployeeById(Long id) {
+    return employeeRepository.findEmployeeById(id);
+  }
+  
+}
