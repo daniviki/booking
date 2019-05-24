7ab67e1a3fe358ece3dbf41e1b75715fcffc4389
Karoly Valentin <valentin.karoly@gmail.com>
2019-05-24 11:02:09 +0200

    new query

--- src/main/java/com/chrysoprase/booking/company/CompanyRepository.java
+++ src/main/java/com/chrysoprase/booking/company/CompanyRepository.java
@@ -5,4 +5,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CompanyRepository extends CrudRepository<Company, Long> {
+
+  Company findCompanyById(Long id);
+
 }
